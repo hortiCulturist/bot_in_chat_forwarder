c3479909b4a1d6c64440ae4fac724dabf27a9a17
horticulturist <yurezz007@gmail.com>
2023-02-05 18:12:08 +0200

    add

---
+++ bot.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import log
+import config
+
+app = Client(**log, )
+
+
+@app.on_message(filters.chat(int(config.bot_id)))
+async def new_media_group(client: Client, message: Message):
+    mess = message.text
+    await app.send_message(config.channel, mess)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ config.py
@@ -0,0 +1,11 @@
+log = dict(
+    name='user',
+    device_model='k530i',
+    app_version='555',
+    system_version='23456uiop',
+    api_hash='92d3f8cf7cf27563e676c61d5929863d',
+    api_id=17309257
+)
+
+bot_id = '5567531356'
+channel = '-1001892649519'
